b0ea8ecc0e48647ab640a6fafdd6e7e5e03947b8
Robiul Hasan <robiulhasan9559@gmail.com>
2025-12-06 00:31:03 +0600

    (Feature) Table design updated

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -11,20 +11,20 @@ const TaskList = ({
       <table className="table-fixed xl:w-full">
         <thead>
           <tr>
-            <th className="py-5 px-4 font-semibold capitalize text-base bg-neutral-200 text-start w-[48px]"></th>
+            <th className="py-5 px-4 font-semibold capitalize text-base bg-neutral-200 text-start w-[238px]"></th>
             <th className="py-5 px-4 font-semibold capitalize text-base bg-neutral-200 text-start">
               Title
             </th>
-            <th className="py-5 px-4 font-semibold capitalize text-base bg-neutral-200 text-start">
+            <th className="py-5 px-4 font-semibold capitalize text-base bg-neutral-200 text-start w-[238px]">
               Description
             </th>
-            <th className="py-5 px-4 font-semibold capitalize text-base bg-neutral-200 text-center w-[300px]">
+            <th className="py-5 px-4 font-semibold capitalize text-base bg-neutral-200 text-center w-[238px]">
               Tags
             </th>
-            <th className="py-5 px-4 font-semibold capitalize text-base bg-neutral-200 text-start w-[120px]">
+            <th className="py-5 px-4 font-semibold capitalize text-base bg-neutral-200 text-center  w-[238px]">
               Priority
             </th>
-            <th className="py-5 px-4 font-semibold capitalize text-base bg-neutral-200 text-start">
+            <th className="py-5 px-4 font-semibold capitalize text-base bg-neutral-200 text-center w-[238px]">
               Options
             </th>
           </tr>
@@ -33,27 +33,6 @@ const TaskList = ({
 
       <div className="overflow-auto max-h-[400px]">
         <table className="table-fixed overflow-auto xl:w-full">
-          {/* <thead>
-            <tr>
-              <th className="py-5 px-4 font-semibold capitalize text-base bg-neutral-200 text-start w-[48px]"></th>
-              <th className="py-5 px-4 font-semibold capitalize text-base bg-neutral-200 text-start ">
-                Title
-              </th>
-              <th className="py-5 px-4 font-semibold capitalize text-base bg-neutral-200 text-start">
-                Description
-              </th>
-              <th className="py-5 px-4 font-semibold capitalize text-base bg-neutral-200 text-center w-[300px]">
-                Tags
-              </th>
-              <th className="py-5 px-4 font-semibold capitalize text-base bg-neutral-200 text-start w-[120px]">
-                Priority
-              </th>
-              <th className="py-5 px-4 font-semibold capitalize text-base bg-neutral-200 text-start ">
-                Options
-              </th>
-            </tr>
-          </thead> */}
-
           <tbody className="max-h-[300px] overflow-y-auto">
             {tasks.map((task) => (
               <tr
@@ -61,7 +40,7 @@ const TaskList = ({
                 className="border-b border-neutral-300 [&>td]:align-baseline [&>td]:px-4 [&>td]:py-5"
               >
                 {/* Star */}
-                <td className="!align-middle text-start">
+                <td className="w-[238px] !align-middle text-start">
                   <button
                     type="button"
                     onClick={() => onHandleIsFavorite(task.id)}
@@ -81,15 +60,15 @@ const TaskList = ({
                 </td>
 
                 {/* Title */}
-                <td className="!align-middle text-start">{task.title}</td>
+                <td className="w-[238px] !align-middle text-start">{task.title}</td>
 
                 {/* Description */}
-                <td className="!align-middle text-start">
+                <td className="w-[238px] !align-middle text-start">
                   <div>{task.description}</div>
                 </td>
 
                 {/* Tags */}
-                <td className="!align-middle !py-5 px-4">
+                <td className="w-[238px] !align-middle !py-5 px-4">
                   <ul className="flex justify-center gap-1.5 flex-wrap">
                     {task.tags.map((tag, index) => (
                       <li key={index}>
@@ -102,9 +81,9 @@ const TaskList = ({
                 </td>
 
                 {/* Priority */}
-                <td className="!align-middle text-start">
+                <td className="w-[238px] !align-middle text-center">
                   <span
-                    className={`px-4 py-1 text-sm font-semibold rounded flex justify-center capitalize
+                    className={`px-4 py-1 text-sm font-semibold rounded inline-flex justify-center capitalize
                       ${
                         task.priority === "high" &&
                         "text-green-600 bg-green-100"
@@ -121,7 +100,7 @@ const TaskList = ({
                 </td>
 
                 {/* Action Buttons */}
-                <td className="!align-middle !py-5 px-4">
+                <td className="w-[238px] !align-middle !py-5 px-4">
                   <div className="flex items-center justify-center space-x-3">
                     <button
                       className="w-8 h-8 rounded-[6px] flex justify-center items-center text-white active:scale-95 bg-red-500 hover:bg-red-600"
